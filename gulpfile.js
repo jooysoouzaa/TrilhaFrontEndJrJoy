3bc1eb3d2e7f8974c8197a6d8fd10d35b2b806de
Joy Sóuza <joysouza.contato@gmail.com>
2024-07-09 22:31:13 -0300

    Atualizei gulpfile para copiar arquivos HTML para a pasta build

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass')); // Comprimir SASS
+const sass = require('gulp-sass')(require('sass'));
 const sourcemaps = require('gulp-sourcemaps');
-const imagemin = require('gulp-imagemin'); // Comprimir imagens
+const imagemin = require('gulp-imagemin');
 
+// Função para comprimir imagens
 function comprimeImagens() {
     return gulp.src('./source/images/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'));
 }
 
+// Função para compilar SASS
 function compilaSass() {
     return gulp.src('./source/styles/style.scss')
         .pipe(sourcemaps.init())
@@ -19,11 +21,17 @@ function compilaSass() {
         .pipe(gulp.dest('./build/styles'));
 }
 
-//Tarefa de build para Vercel
-gulp.task('build', gulp.parallel(compilaSass, comprimeImagens));
+// Função para copiar arquivos HTML
+function copiaHtml() {
+    return gulp.src('./source/*.html')
+        .pipe(gulp.dest('./build'));
+}
 
+// Tarefa de build para Vercel
+gulp.task('build', gulp.parallel(compilaSass, comprimeImagens, copiaHtml));
 
 exports.default = function () {
     gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
     gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImagens));
-};
+    gulp.watch('./source/*.html', { ignoreInitial: false }, gulp.series(copiaHtml));
+}
